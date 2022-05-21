78ceb5d2887d1e0ca26dca5e44915579b63da78d
Theoneste <theoneste2060.com>
2022-05-21 07:02:27 -0700

    added important links and navbar with footer

--- dashboard.php
+++ dashboard.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="style.css">
+    <title>Login </title>
 </head>
-<body>
-    <p>hello world</p>
+<body class="mr-5 ml-5">
+
+<div class="row pt1 pb-1" style="background:skyblue; color:#fff;font-weight:600">
+<div class="col-md-1">About us</div>
+<div class="col-md-1">Advertisement</div>
+<div class="col-md-6"><center>Kigali, Rwanda</center></div>
+<div class="col-md-2">How search work</div>
+<div class="col-md-1">Privacy</div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 </html>
