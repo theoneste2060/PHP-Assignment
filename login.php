37976f39cd23ef9bbb15f2fb7dce31ff5e298a89
Theoneste <theoneste2060.com>
2022-05-21 06:19:37 -0700

    created a login form

--- login.php
+++ login.php
@@ -12,7 +12,24 @@
 </head>
 <body class="mr-5 ml-5">
 <div class="row pt1 pb-1" style="background:red; color:#fff;font-weight:600"><center>Assignment</center> </div>
-
+<div class="container mt-5 mb-5 d-flex justify-content-center">
+    <div class="card px-1 py-4">
+        <div class="card-body">
+            <h3 class="card-title mb-3 pb-3"><center><b>Welcome back!</b></center></h3>
+            <h6 class="card-title mb-3 pb-3"><center>Sign In</center></h6>
+            
+           <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="E-mail" name="email"> </div>
+                </div>
+            </div>
+            <div class=" d-flex flex-column text-center px-5 mt-3 mb-1"> </div> <button class="btn btn-primary btn-block confirm-button">Next</button><hr>
+            <div class=" d-flex flex-column text-center px-5 mt-3 mb-1"></div> <button class="btn btn-danger btn-block confirm-button">Continue with google</button>
+        <div> <a href="#" class="terms mt-3" style="color:skyblue;"><b>Create account</b></a> </div>
+        </div>
+    </div>
+</div>
 <div class="row pt1 pb-1" style="background:skyblue; color:#fff;font-weight:600">
 <div class="col-md-1">About us</div>
 <div class="col-md-1">Advertisement</div>
