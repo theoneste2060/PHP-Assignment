f4af9eaa26d58f6c90c9b6b64cb0183ba4598deb
Theoneste <theoneste2060.com>
2022-05-21 09:42:43 -0700

    added profile descriptions and reset form

--- dashboard.php
+++ dashboard.php
@@ -14,18 +14,48 @@
 <div class="row pt1 pb-1" style="background:red; color:#fff;font-weight:600"><center>Assignment</center> </div>
 <div class="row pt-2 " style="display:fex; " >
     <div class="col-md-8">
-        <div>
+        <div class="">
         <div class="position-relative" style="height:25vh; background: url(Picture2.png); background-size: cover; background-repeat: no-repeat">
         <span class="position-absolute bottom-0 start-0 ml-2">
-             <img class="img pt-5" src="Picture1.png" alt="">
+             <img class="img pt-5 " src="Picture1.png" alt="">
         </span>
         </div>
-        
-    </div>
-        <div>headline</div>
-        <div>
-            form
+       
+        <div class="mt-5">
+             <hr>
+            <div class="col-sm-4 ">
+           
+                <h6><center>Headline</center></h6>
+            </div>
+            <span>Software Engineer | Data analyst</span>
         </div>
+        <div class="row ">
+        <a href="" class="btn border-danger border-top-0 border-right-0 border-left-0 bg-primary col-sm-12 mt-5" style="color:white; font-size:20px ;text-align:left; ">Change pasword</a>
+        </div>
+        <div class="container mt-5 mb-5 d-flex justify-content-center">
+            <div class="card px-1 py-4">
+                <div class="card-body">
+                        <div class="row d-flex flex-row">
+                        <div class="col-sm-6"><label for=""><b>Current password</b></label></div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" type="password" placeholder="password" name="password"> </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                        <div class="col-sm-6"><label for=""><b>New password</b></label></div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <input class="form-control" type="password" placeholder="password" name="password"> </div>
+                            </div>
+                        </div>
+                        <div> <input type="checkbox" class="terms mt-3" style="color:skyblue;">Remind me to change password for 3 months </div>
+                    <div class=" d-flex flex-column text-center  mt-2 mb-1"> </div> <button class="btn btn-primary btn-block confirm-button">Set new password</button>
+        
+        </div>
+    </div>
+</div>
+    </div> 
     </div>
     <div class="col-md-4 " >
         <ul style="list-style:none; text-decoration:none">
