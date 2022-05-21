543591b86c5b9447be202d4a9ec6c0106092ea49
Theoneste <theoneste2060.com>
2022-05-21 06:12:35 -0700

    added navbar and footer as well

--- login.php
+++ login.php
@@ -8,10 +8,18 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="style.css">
-    <title>Login page</title>
+    <title>Login </title>
 </head>
 <body class="mr-5 ml-5">
+<div class="row pt1 pb-1" style="background:red; color:#fff;font-weight:600"><center>Assignment</center> </div>
 
+<div class="row pt1 pb-1" style="background:skyblue; color:#fff;font-weight:600">
+<div class="col-md-1">About us</div>
+<div class="col-md-1">Advertisement</div>
+<div class="col-md-6"><center>Kigali, Rwanda</center></div>
+<div class="col-md-2">How search work</div>
+<div class="col-md-1">Privacy</div>
+</div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 </html>
